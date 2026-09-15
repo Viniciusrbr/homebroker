a2a6244e8a64b5d7470414134986396837c17702
viniciusrbr <viniciusgoes159@gmail.com>
2026-09-15 20:38:31 -0300

    feat: implementar apresentação de ativos e integração com o controlador de ativos

--- nestjs-api/src/wallets/entities/wallet.entity.ts
+++ nestjs-api/src/wallets/entities/wallet.entity.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import crypto from "crypto";
-import { HydratedDocument } from "mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
+import { WalletAsset, WalletAssetDocument } from "./wallet-asset.entity.js";
 
 export type WalletDocument = HydratedDocument<Wallet>;
 
@@ -8,6 +9,14 @@ export type WalletDocument = HydratedDocument<Wallet>;
 export class Wallet {
   @Prop({ default: () => crypto.randomUUID() })
   _id: string;
+
+  @Prop({
+    type: [mongoose.Schema.Types.String],
+    // proteção contra duplicatas, garantindo que cada ativo seja único dentro da carteira
+    set: (v: (WalletAssetDocument | string)[]) => [...new Set(v)],
+    ref: WalletAsset.name,
+  })
+  assets: WalletAssetDocument[] | string[];
 
   createdAt!: Date;
   updatedAt!: Date;
--- nestjs-api/src/wallets/wallets.service.ts
+++ nestjs-api/src/wallets/wallets.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { CreateWalletDto } from './dto/create-wallet.dto.js';
-import { InjectModel } from '@nestjs/mongoose';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { Wallet } from './entities/wallet.entity.js';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { WalletAsset } from './entities/wallet-asset.entity.js';
 
 @Injectable()
@@ -10,7 +10,8 @@ export class WalletsService {
 
   constructor(
     @InjectModel(Wallet.name) private walletSchema: Model<Wallet>,
-    @InjectModel(WalletAsset.name) private walletAssetSchema: Model<WalletAsset>
+    @InjectModel(WalletAsset.name) private walletAssetSchema: Model<WalletAsset>,
+    @InjectConnection() private connection: mongoose.Connection
   ) { }
 
   create(createWalletDto: CreateWalletDto) {
@@ -22,14 +23,49 @@ export class WalletsService {
   }
 
   findOne(id: string) {
-    return this.walletSchema.findById(id);
+    return this.walletSchema.findById(id).populate([
+      {
+        path: 'assets',
+        populate: ['asset']
+      }
+    ]);
   }
 
-  createWalletAsset(data: { walletId: string, assetId: string, shares: number }) {
-    return this.walletAssetSchema.create({
-      wallet: data.walletId,
-      asset: data.assetId,
-      shares: data.shares
-    });
+  async createWalletAsset(data: {
+    walletId: string;
+    assetId: string;
+    shares: number;
+  }) {
+
+    const session = await this.connection.startSession();
+    await session.startTransaction();
+
+    try {
+      const docs = await this.walletAssetSchema.create(
+        [{
+          wallet: data.walletId,
+          asset: data.assetId,
+          shares: data.shares,
+        },],
+        { session },
+      );
+
+      const walletAsset = docs[0];
+
+      await this.walletSchema.updateOne(
+        { _id: data.walletId },
+        { $push: { assets: walletAsset._id } },
+        { session, },
+      );
+
+      await session.commitTransaction();
+      return walletAsset;
+    } catch (e) {
+      console.error(e);
+      await session.abortTransaction();
+      throw e;
+    } finally {
+      await session.endSession();
+    }
   }
 }
