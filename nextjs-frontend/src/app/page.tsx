7224e3de97c85bed89093b67cf3822c45f8a5daa
viniciusrbr <viniciusgoes159@gmail.com>
2026-09-17 17:39:23 -0300

    feat: implementar funcionalidades de carteira e ativos com tabelas e alertas

--- nextjs-frontend/src/app/page.tsx
+++ nextjs-frontend/src/app/page.tsx
@@ -1,69 +1,65 @@
-import Image from "next/image";
+import { AssetShow } from "@/components/AssetShow";
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { WalletList } from "@/components/WalletList";
+import { getMyWallet } from "@/queries/queries";
 
-export default function Home() {
+
+export default async function MyWalletListPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ wallet_id: string }>;
+}) {
+  const { wallet_id } = await searchParams;
+
+  if (!wallet_id) {
+    return <WalletList />;
+  }
+
+  const wallet = await getMyWallet(wallet_id);
+
+  if (!wallet) {
+    return <WalletList />;
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="flex flex-col space-y-5 grow">
+      <article className="format">
+        <h1>Minha carteira</h1>
+      </article>
+
+      <div className="overflow-x-auto w-full">
+        <Table>
+          <TableCaption>Detalhes da sua carteira</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Ativo</TableHead>
+              <TableHead>Cotação</TableHead>
+              <TableHead>Quantidade</TableHead>
+              <TableHead>Comprar/vender</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {
+              wallet.assets.map((walletAsset, key) => (
+                <TableRow key={key}>
+                  <TableCell>
+                    <AssetShow asset={walletAsset.asset} />
+                  </TableCell>
+                  <TableCell>R$ {walletAsset.asset.price}</TableCell>
+                  <TableCell>{walletAsset.shares}</TableCell>
+                  <TableCell>
+                    <Button
+                    >
+                      Comprar/vender
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))
+            }
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
---
+++ nextjs-frontend/src/components/WalletList.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { getWallets } from "@/queries/queries";
+
+export async function WalletList() {
+  const wallets = await getWallets();
+
+  return (
+    <div className="flex flex-col space-y-5 grow">
+      <Alert variant="destructive">
+        <AlertTitle>Nenhuma wallet escolhida</AlertTitle>
+      </Alert>
+
+      <article className="format">
+        <h1>Carteiras existentes</h1>
+      </article>
+
+      <div className="overflow-x-auto w-full">
+        <Table className="table-fixed">
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>Acessar</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {wallets.map((wallet) => (
+              <TableRow key={wallet._id}>
+                <TableCell>{wallet._id}</TableCell>
+                <TableCell>
+                  <Button
+                    variant="link"
+                    render={<Link href={`/?wallet_id=${wallet._id}`} />}
+                  >
+                    Acessar
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
