42459fbeef03f8435721fe33f487e71b194aaf3a
viniciusrbr <viniciusgoes159@gmail.com>
2026-09-24 14:48:21 -0300

    feat: implementar AssetsGateway e integrar eventos de preço com WebSocket

---
+++ nestjs-api/src/assets/assets.gateway.ts
@@ -0,0 +1,51 @@
+import { OnGatewayInit, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { AssetsService } from './assets.service.js';
+import { Logger } from '@nestjs/common';
+import { AssetPresenter } from './asset.presenter.js';
+
+@WebSocketGateway({ cors: true })
+export class AssetsGateway implements OnGatewayInit {
+  logger = new Logger(AssetsGateway.name);
+  constructor(private assetsService: AssetsService) { }
+
+  afterInit(server: any) {
+    this.assetsService.subscribeNewPriceChangedEvents().subscribe((asset) => {
+      server.to(asset.symbol).emit('assets/price-changed', new AssetPresenter(asset).toJSON());
+    });
+  }
+
+  @SubscribeMessage('joinAssets')
+  handleJoinAssets(client: any, payload: { symbols: string[] }) {
+    if (!payload.symbols?.length) {
+      return;
+    }
+    payload.symbols.forEach((symbol) => {
+      client.join(symbol);
+    });
+    this.logger.log(`Client ${client.id} joined assets ${payload.symbols.join(', ')}`);
+  }
+
+  @SubscribeMessage('joinAsset')
+  handleJoinAsset(client: any, payload: { symbol: string }) {
+    client.join(payload.symbol);
+    this.logger.log(`Client ${client.id} joined asset ${payload.symbol}`);
+  }
+
+  @SubscribeMessage('LeaveAssets')
+  handleLeaveAssets(client: any, payload: { symbols: string[] }) {
+    if (!payload.symbols?.length) {
+      return;
+    }
+    payload.symbols.forEach((symbol) => {
+      client.leave(symbol);
+    });
+    this.logger.log(`Client ${client.id} left assets ${payload.symbols.join(', ')}`);
+  }
+
+  @SubscribeMessage('LeaveAsset')
+  handleLeaveAsset(client: any, payload: { symbol: string }) {
+    client.leave(payload.symbol);
+    this.logger.log(`Client ${client.id} left asset ${payload.symbol}`);
+  }
+
+}
--- nestjs-api/src/assets/assets.service.ts
+++ nestjs-api/src/assets/assets.service.ts
@@ -3,6 +3,7 @@ import { CreateAssetDto } from './dto/create-asset.dto.js';
 import { InjectModel } from '@nestjs/mongoose';
 import { Asset } from './entities/asset.entity.js';
 import { Model } from 'mongoose';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AssetsService {
@@ -26,4 +27,44 @@ export class AssetsService {
 
     return asset;
   }
+
+  // subscribeNewPriceChangedEvents(): Observable<Asset> {
+  //   return new Observable((observer) => {
+  //     this.assetSchema.watch([
+  //       {
+  //         $match: {
+  //           $or: [{ operationType: 'update' }, { operationType: 'replace' }]
+  //         }
+  //       }
+  //     ],
+  //       { fullDocument: 'updateLookup', fullDocumentBeforeChange: 'whenAvailable' },
+  //     ).on('change', async (data) => {
+  //       if (data.fullDocument.price === data.fullDocumentBeforeChange.price) {
+  //         return;
+  //       }
+  //       const asset = await this.assetSchema.findById(data.fullDocument._id);
+  //       observer.next(asset!);
+  //     });
+  //   })
+  // }
+
+  subscribeNewPriceChangedEvents(): Observable<Asset> {
+    return new Observable((observer) => {
+      this.assetSchema.watch(
+        [
+          {
+            $match: {
+              operationType: 'update',
+              'updateDescription.updatedFields.price': { $exists: true },
+            },
+          },
+        ],
+        { fullDocument: 'updateLookup' },
+      ).on('change', (data) => {
+        observer.next(data.fullDocument);
+      });
+    });
+  }
+
+
 }
